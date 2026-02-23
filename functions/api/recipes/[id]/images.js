1df3acd8b05fa1cb529b7730536e847fc8c629c5
纸上滑雪 <zlpr123456@github.com>
2026-02-23 15:47:12 +0800

    修复images.js中的拼写错误

--- functions/api/recipes/[id]/images.js
+++ functions/api/recipes/[id]/images.js
@@ -17,7 +17,7 @@ export async function onRequestGet(context) {
   } catch (error) {
     return new Response(JSON.stringify({
       success: false,
-      error: error.MESSAge
+      error: error.message
     }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' }
